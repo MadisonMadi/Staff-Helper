c3b133a4b9aa93eca0b5a69d9cd4bf99806efa18
MadisonMadi <thedarkmoonminrecraft@gmail.com>
2025-07-07 16:33:25 +0200

    Older Fabric Loader/Cloth Config/Mod Menu Compatibilty Added Config Support Added more help messages Changed sendAsplayer Updated README

--- src/main/java/me/Madisonn/StaffHelperMod/CommandRegistry.java
+++ src/main/java/me/Madisonn/StaffHelperMod/CommandRegistry.java
@@ -1,134 +1,95 @@
 package me.Madisonn.StaffHelperMod;
 
+import me.Madisonn.StaffHelperMod.Config.StaffHelperConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
 
 public class CommandRegistry implements ClientModInitializer {
+    public static StaffHelperConfig CONFIG;
+
     @Override
     public void onInitializeClient() {
+        AutoConfig.register(StaffHelperConfig.class, GsonConfigSerializer::new);
+        CONFIG = AutoConfig.getConfigHolder(StaffHelperConfig.class).getConfig();
+
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("staffhelp")
                 .then(ClientCommandManager.literal("bait")
-                        .executes(context -> {
-                            sendAsPlayer("To use bait press 'Q' while holding your fishing rod, click the tackle box and drop your baits in there!");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.baitMessage)))
 
                 .then(ClientCommandManager.literal("baitpack")
-                        .executes(context -> {
-                            sendAsPlayer("To get bait packages you can do /vote, purchase them in /ah or from Fish Compendium and '/collections'.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.baitpackMessage)))
 
                 .then(ClientCommandManager.literal("petdrop")
-                        .executes(context -> {
-                            sendAsPlayer("Pet Drop Chances are Common 1/750 - Rare 1/4000 - Epic 1/10000 - Legendary 1/30000 - Mythical 1/80000!");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.petdropMessage)))
 
                 .then(ClientCommandManager.literal("armor")
-                        .executes(context -> {
-                            sendAsPlayer("Shards are used for armor with default armor recipes, no helmets.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.armorMessage)))
 
                 .then(ClientCommandManager.literal("variants")
-                        .executes(context -> {
-                            sendAsPlayer("Albino 1/5000, Melanistic 1/10000 and Trophy 1/15000.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.variantsMessage)))
 
                 .then(ClientCommandManager.literal("artisan")
-                        .executes(context -> {
-                            sendAsPlayer("To craft or upgrade rod pieces you can go to the artisan at spawn or use '/artisan' (Angler Rank).");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.artisanMessage)))
 
                 .then(ClientCommandManager.literal("forge")
-                        .executes(context -> {
-                            sendAsPlayer("To tier up your armor you can go to the forge at spawn or use '/forge' (Sailor Rank), T2 16 Shards, T3 48 Shards, T4 96 Shards, T5 256 Shards.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.forgeMessage)))
 
                 .then(ClientCommandManager.literal("scrapper")
-                        .executes(context -> {
-                            sendAsPlayer("To scrap your armor pieces you can go to the scrapper at spawn or use '/scrapper' to scrap your pieces and get some shards in return! '/scrapper'.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.scrapperMessage)))
 
                 .then(ClientCommandManager.literal("auction")
-                        .executes(context -> {
-                            sendAsPlayer("You can buy and sell items in the '/ah', there's a small tax when listing an item '/ah sell price'");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.auctionMessage)))
 
                 .then(ClientCommandManager.literal("cosmetics")
-                        .executes(context -> {
-                            sendAsPlayer("Cosmetics can be bought with credits from '/buy' these are purely cosmetic and will not influence your gameplay.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.cosmeticsMessage)))
 
                 .then(ClientCommandManager.literal("crew")
-                        .executes(context -> {
-                            sendAsPlayer("You can create a crew '/crew create name' a crew island is mostly used for building and storage! crews are also used to compete for fun!");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.crewMessage)))
 
                 .then(ClientCommandManager.literal("foe")
-                        .executes(context -> {
-                            sendAsPlayer("You can install the FishonMC-Extras Mod here modrinth.com/mod/fishonmc-extras.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.foeMessage)))
 
                 .then(ClientCommandManager.literal("luckscaleprospect")
-                        .executes(context -> {
-                            sendAsPlayer("Luck boosts fish rarity chance, Scale boosts bigger fish chance, Prospect boosts shard chance.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.luckscaleprospectMessage)))
 
                 .then(ClientCommandManager.literal("reelbiteline")
-                        .executes(context -> {
-                            sendAsPlayer("Reel Speed increases progress per tick, Bite Speed reduces wait time for bites.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.reelbitelineMessage)))
 
                 .then(ClientCommandManager.literal("xpmoney")
-                        .executes(context -> {
-                            sendAsPlayer("Bonus XP boosts player, location, and crew XP. Bonus Money increases earnings from catches.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.xpmoneyMessage)))
 
                 .then(ClientCommandManager.literal("howfish")
-                        .executes(context -> {
-                            sendAsPlayer("Sneak to raise line tension (blue bar right), stop to lower it (left). Fill the green bar to catch the fish!");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.howfishMessage)))
 
                 .then(ClientCommandManager.literal("tutorial")
-                        .executes(context -> {
-                            sendAsPlayer("You can use the command '/tasks' to see a quick tutorial which will give you the rundown on the basics of the gameplay loop.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.tutorialMessage)))
 
                 .then(ClientCommandManager.literal("calibrator")
-                        .executes(context -> {
-                            sendAsPlayer("Calibrate reels and poles for better stats and bonuses. Higher rarity increases costs and bonus percentage. Upgrading in Artisan removes bonus. '/calibrator' (Mariner Rank)");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.calibratorMessage)))
 
                 .then(ClientCommandManager.literal("locationroll")
-                        .executes(context -> {
-                            sendAsPlayer("Each location has a bonus roll per level 1-20, slots can be rolled for Species Size Bonus, General Bonus: luck, prospect and scale, also Sell and XP bonus.");
-                            return 1;
-                        }))
+                        .executes(context -> sendAsPlayer(CONFIG.locationrollMessage)))
+
+                .then(ClientCommandManager.literal("quests")
+                        .executes(context -> sendAsPlayer(CONFIG.questsMessage)))
+
+                .then(ClientCommandManager.literal("earnmoney")
+                        .executes(context -> sendAsPlayer(CONFIG.earnmoneyMessage)))
+
+                .then(ClientCommandManager.literal("wiki")
+                        .executes(context -> sendAsPlayer(CONFIG.wikiMessage)))
         ));
     }
 
-    private void sendAsPlayer(String message) {
+    private int sendAsPlayer(String message) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.player != null && client.getNetworkHandler() != null) {
             client.getNetworkHandler().sendChatMessage(message);
         }
+        return 1;
     }
 }
---
+++ src/main/java/me/Madisonn/StaffHelperMod/Config/ModMenuIntegration.java
@@ -0,0 +1,13 @@
+package me.Madisonn.StaffHelperMod.Config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(StaffHelperConfig.class, parent).get();
+    }
+}
