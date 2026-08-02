968d11f8cf60878406681176b031fd779a3e63b8
MadisonMadi <17278366+MadisonMadi@users.noreply.github.com>
2026-08-02 21:27:10 +0200

    quick fix

--- src/main/java/madisonn/staffhelpermod/config/Configs.java
+++ src/main/java/madisonn/staffhelpermod/config/Configs.java
@@ -4,8 +4,7 @@ import me.fzzyhmstrs.fzzy_config.api.ConfigApiJava;
 import me.fzzyhmstrs.fzzy_config.api.RegisterType;
 
 public class Configs {
-    public static final MessagesConfig messages =
-            ConfigApiJava.registerAndLoadConfig(MessagesConfig::new, RegisterType.CLIENT);
+    public static MessagesConfig messages = ConfigApiJava.registerAndLoadConfig(MessagesConfig::new, RegisterType.CLIENT);
 
     public static void init() {}
 }
